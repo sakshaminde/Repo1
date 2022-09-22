742acd424effbe98d98128a68cce9458d9f6b2fc
sakshaminde <sakshaminde3454@gmail.com>
2022-09-22 20:39:48 +0530

    My Third Commit 3

---
+++ project3-backend/src/test/java/com/kpi/project3/project3/AppTest.java
@@ -0,0 +1,12 @@
+package com.kpi.project3.project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
