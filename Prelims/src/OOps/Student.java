4301040594453ece8c18d750c75e0dbe1d682429
sakshaminde <sakshaminde3454@gmail.com>
2022-09-26 14:16:47 +0530

    New of Inheritance.

---
+++ Prelims/src/OOps/Student.java
@@ -0,0 +1,10 @@
+package OOps;
+
+public class Student extends School {
+
+	@Override
+	int salary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ Prelims/src/OOps/Teacher.java
@@ -0,0 +1,11 @@
+package OOps;
+
+public class Teacher extends School {
+
+	@Override
+	int salary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
